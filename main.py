e42ea9823ae048bfb13ee65c7b628fbf2ab5be3a
Luis Villén <l.villenjaldon@gmail.com>
2020-12-21 21:23:09 +0100

    Excepción de errores

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import requests
 API_KEY = "2133bec5"
 url_template = "http://www.omdbapi.com/?apikey={}&{}={}"
 
+class PeticionError(Exception):
+    pass
+
 def peticion(url):
     respuesta = requests.get(url)
     if respuesta.status_code == 200:
         datos = respuesta.json()
         if datos["Response"] == "False":
-            return datos["Error"]
+            raise PeticionError(datos["Error"])
         else: 
             return datos
     else:
-        return "Error en consulta por id: ", respuesta.status_code
+        raise  PeticionError("Error en consulta: {}".format(respuesta.status_code))
 
 repetir = 'S'
 
 while repetir == 'S':
     pregunta = input("Título de la película: ")
+
     respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
     if isinstance(respuesta, str):
         print(respuesta)
@@ -33,4 +37,5 @@ while repetir == 'S':
             agno = respuesta["Year"]
             director = respuesta["Director"]
             print(f"La peli {titulo}, estrenada en el año {agno}, fue dirigida por {director}")
-    repetir = input("¿Quieres hacer otra búsqueda? S/N ")
+
+    repetir = input("¿Quieres hacer otra búsqueda? (S/N) ").upper()
